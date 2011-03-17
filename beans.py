4e421b171e949cc04fbe1b1b7605287df089e76c
Josh Ourisman <josh@joshourisman.com>
2011-03-17 15:29:08 -0400

    count gets score, incr increments score

--- beans.py
+++ beans.py
@@ -4,9 +4,13 @@ from redis import Redis
 app = Flask(__name__)
 db = Redis(db='beans')
 
+@app.route('/incr/<key>/<member>/')
+def incr(key, member):
+    return jsonify(count = db.zincrby(key, member))
+
 @app.route('/count/<key>/<member>/')
 def count(key, member):
-    return jsonify(count = db.zincrby(key, member))
+    return jsonify(count = db.zscore(key, member))
 
 if __name__ == "__main__":
     app.debug = True
